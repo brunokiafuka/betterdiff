34465f42c08d958f30dced2c181e219fd5b18167
Bruno Kiafuka <brunokiafuka@live.com.pt>
2026-01-26 20:52:27 +0000

    Add JWT key generation script
    - Introduce a new script to generate private and public keys for JWT authentication
    - Utilize the 'jose' library for key pair generation and export
    - Output keys in a format suitable for GitHub signup integration

---
+++ scripts/generateJWTKeys.mjs
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import { exportJWK, exportPKCS8, generateKeyPair } from "jose";
+
+/**
+ * Generate keys used Private and Public keys for Github signup
+ * */
+async function generateKeys() {
+  const keys = await generateKeyPair("RS256", {
+    extractable: true,
+  });
+  const privateKey = await exportPKCS8(keys.privateKey);
+  const publicKey = await exportJWK(keys.publicKey);
+  const jwks = JSON.stringify({ keys: [{ use: "sig", ...publicKey }] });
+
+  process.stdout.write(
+    `JWT_PRIVATE_KEY="${privateKey.trimEnd().replace(/\n/g, " ")}"`,
+  );
+  process.stdout.write("\n");
+  process.stdout.write(`JWKS=${jwks}`);
+  process.stdout.write("\n");
+}
+
+generateKeys().catch(console.error);
